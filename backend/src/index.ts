87c40a1cdf57d04753299180a5efcdda1d7712b5
arttu <ttu91@hotmail.com>
2023-11-13 21:40:52 +0200

    Clear and seed test data on dev startup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
 import { PORT } from "./util/config.js";
 import { connectToDatabase } from "./util/db.js";
 import { app } from "./app.js";
+import { seedTestData } from "./testUtils/seedTestData.js";
 
 const start = async () => {
   await connectToDatabase();
+  await seedTestData();
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
